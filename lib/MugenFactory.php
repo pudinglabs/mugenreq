86284794d55a11c601568634467c0cd4b000e766
Mustoharin <mustoharin.id@myrepublic.net>
2018-11-12 14:57:10 +0700

    adding zklib maybe work

--- lib/MugenFactory.php
+++ lib/MugenFactory.php
@@ -4,6 +4,7 @@ namespace mugenreq;
 
 use mugenreq\Mugen;
 use mugenreq\MugenSoap;
+use mugenreq\TADZKLib;
 
 
 class MugenFactory
@@ -24,12 +25,13 @@ class MugenFactory
         
         $soap_options = [
             'location' => "http://{$options['ip']}/iWsService",
-            'port' => 80,
-            'connection_timeout' => $options['connection_timeout']
+            'connection_timeout' => $options['connection_timeout'],
+            'soap_port' => $options['soap_port']
         ];
 
         return new Mugen(
             new MugenSoap($soap_options),
+            new TADZKLib($options),
             $options
         );
     }
@@ -38,9 +40,13 @@ class MugenFactory
     private function get_default_options()
     {
         $default_options['ip'] = '127.0.0.1';
+        $default_options['internal_id'] = 1;
         $default_options['com_key'] = 123456;
         $default_options['encoding'] = 'iso8859-1';
         $default_options['connection_timeout'] = 5;
+        $default_options['soap_port'] = 80;
+        $default_options['udp_port'] = 4370;
+
         return $default_options;
     }
 
